edd4fb0141b824c13c0ff181c52915388681ba2a
Pedro Henrique Lopes <92961068+PedroLopes7K@users.noreply.github.com>
2022-03-30 18:25:41 -0300

    Delete moduloSys.py

--- moduloSys.py
+++
@@ -1,8 +0,0 @@
-import sys
-while True:
-    print('Digite "Exit" para sair')
-    resposta = input()
-    if resposta == "Exit":
-        print('Saiu do Programa')
-        sys.exit()
-    print('Continua Rodando')
